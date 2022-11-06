9ddf0d7ff68898e5cf8df83aa6a832b0384e3fd9
Omer <omerzahidali@gmail.com>
2022-11-06 14:33:58 +0300

    Refactored to 2 indents + Changed it to Object type

--- main.rb
+++ main.rb
@@ -1,19 +1,30 @@
-def caesar_cipher(string, shift)
-    letters = ('a'..'z').to_a
-    cap_letters = ('A'.. 'Z').to_a
+class Code
+  def initialize(string)
+    @string = string
+    @letters = ('a'..'z').to_a
+    @cap_letters = ('A'.. 'Z').to_a
+  end
+
+  def cipher(shift)
     result = ''
-    for letter in string.split('')
-        unless letters.include?(letter) || cap_letters.include?(letter) # If it's a non-letter
-            result += letter
+    for letter in @string.split('')
+      unless @letters.include?(letter) || @cap_letters.include?(letter) # If it's a non-letter
+        result += letter
+      else
+        if letter.upcase == letter
+          result += @cap_letters[(@cap_letters.find_index(letter) + shift) % 26]
         else
-            if letter.upcase == letter
-                result += cap_letters[(cap_letters.find_index(letter) + shift) % 26]
-            else
-                result += letters[(letters.find_index(letter) + shift) % 26]
-            end
+          result += @letters[(@letters.find_index(letter) + shift) % 26]
         end
+      end
     end
     return result
+  end
+
+  def decipher(shift)
+    result = ''
+  end
 end
 
-caesar_cipher("What a string!", 5)
+code = Code.new("What a string!")
+puts code.cipher(5)
